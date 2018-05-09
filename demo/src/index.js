a72b2acd0995b49f051085ba1d1938e53cc9312e
Josh Jarmain <jjarmain@gmail.com>
2018-05-08 20:15:25 -0700

    testing out with demo

--- demo/src/index.js
+++ demo/src/index.js
@@ -6,15 +6,58 @@ import registerServiceWorker from './registerServiceWorker';
 import { createReducer, generationDefinition } from 'redux-drive';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-let manifest = {
-  HELLO_WORLD: { payload: { message: 'hello world' } },
+let manifest1 = {
+  HELLO_WORLD: {
+    payload: {
+      message: 'hello world',
+    },
+  },
+  ACTION_1: {
+    payload: (one, two) => ({
+      stuff: {
+        thing: {
+          what: one,
+          hey: {
+            two,
+          },
+        },
+      },
+    }),
+  },
 };
-let definitions = generationDefinition(manifest);
-console.log(definitions);
-let reducer = createReducer(definitions, { heck: 'yes' });
+let manifest2 = {
+  CHICKEN_DINNER: {
+    payload: {
+      hey: 'mondays',
+    },
+  },
+  UPDATE_STUFF: {
+    payload: {
+      stuff: 'asdf',
+    },
+    reduce: (state, payload) => {
+      let stuff = {
+        stuff: payload.stuff,
+      };
+      return {
+        ...state,
+        ...stuff,
+      };
+    },
+  },
+};
+let definitions1 = generationDefinition(manifest1);
+let definitions2 = generationDefinition(manifest2);
+
+let reducer = createReducer([definitions1, definitions2], {
+  heck: 'yes',
+});
 let store = createStore(reducer);
-console.log(definitions.actions.helloWorld());
-store.dispatch(definitions.actions.helloWorld());
+
+store.dispatch(definitions1.actions.helloWorld());
+store.dispatch(definitions2.actions.chickenDinner());
+store.dispatch(definitions1.actions.action1('one', 'two'));
+store.dispatch(definitions2.actions.updateStuff());
 window.store = store;
 ReactDOM.render(
   <Provider store={store}>
